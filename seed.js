42e2e82089fee50b4675ad9e48b0052d7742044e
gjeremy29 <gjeremy0229@outlook.es>
2023-07-24 22:49:06 -0500

    cart

--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ const products = require('./products');
 
 
 const stripe = Stripe
-('sk_test_51NW9L7BWfTVzBxTbU1Jjv4tABzC8qduMxVwSIp5AIcf8dREVu1yvKxlGvsaSNofeohI7zcy6g2Mm6fQfigi8iDwC008Form6ua');
+('process.env.STRIPE_SECRET_KEY');
 
 
 (async () => {
